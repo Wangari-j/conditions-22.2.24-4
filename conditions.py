8ba1ba0e9fe8019e826d543f28a16740838f2d31
Wangari-j <wangarijudy@gmail.com>
2024-02-22 06:12:58 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,21 @@
+def is_same_num(num1,num2):
+    if num1 == num2:
+        print("True")
+    else:
+        print("False")
+    
+is_same_num(2,6)
+is_same_num(9,8)
+is_same_num(5,5)
+
+#2
+
+def numFunc(a,b):
+    if a + b == 10 or b == 10 or a+b ==10:
+        print("True")
+    else:
+        ("False")
+
+numFunc(9,10)
+numFunc(9,9)
+numFunc(1,9)
